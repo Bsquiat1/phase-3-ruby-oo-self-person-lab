57395a452d054b9f4e4584e4e41e57238ee12390
Denis <denis.were@student.moringaschool.com>
2023-05-21 13:31:10 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
-# your code goes here
+require 'pry'
+
+class Person 
+    attr_accessor :name, :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = limit_value(value, 0, 10)
+    end
+
+    def hygiene=(value)
+        @hygiene = limit_value(value, 0, 10)
+    end
+
+    private
+
+    def limit_value(value, min, max)
+        if value > max
+            max
+        elsif value < min
+            min
+        else
+            value
+        end
+    end
+
+
+end
